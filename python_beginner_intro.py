ee6eb9b782b97ccf366d6b2b83f04ed8fed99742
Cedric Arts <152818750+cedricarts@users.noreply.github.com>
2025-10-24 14:28:22 +0200

    Create a simple addition program in Python
    Fixed syntax errors in input statements and added comments.

---
+++ python_beginner_intro.py
@@ -0,0 +1,12 @@
+# Simple Addition Program
+
+# Variable and input declaration
+
+firstNumber = int(input("Enter the first number: ") # Typecasted to integer, so that the input is immediately converted
+secondNumber = int(input("Enter the second number: ")
+
+# Store calculation here
+result = firstNumber + secondNumber #Addition
+
+# Output result to user
+print("Your result is " + result)
